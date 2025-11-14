5f95a89bd0478c354dc659b6971c7b03b3cf4f86
pygamegrupo-eng <pygamegrupo@gmail.com>
2025-11-14 11:51:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys 
+
+Ancho=800
+Alto=600
+
+Fps=60
+
+pygame.init()
+pygame.display.set_caption("FALPY BIRD ")
+screen =pygame.display.set_mode((Ancho,Alto))
+clock=pygame.time.Clock()
+
+ejecutando=True
+while ejecutando:
+  for evento in pygame.event.get():
+    if evento.type==pygame.QUIT:
+      ejecutando=False
+  pygame.display.flip()
+
+  clock.tick(Fps)
+
+pygame.quit()
