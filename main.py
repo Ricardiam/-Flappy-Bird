d59f55fce80565b20ea6e02df419a90a80c25352
pygamegrupo-eng <pygamegrupo@gmail.com>
2025-11-15 17:22:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,59 @@
 import pygame
 import sys 
 
-Ancho=800
-Alto=600
-
-Fps=60
+# Tamaño ventana
+Ancho = 800
+Alto = 600
+Fps = 60
 
 pygame.init()
-pygame.display.set_caption("Gemoetri dash ")
-screen =pygame.display.set_mode((Ancho,Alto))
-clock=pygame.time.Clock()
+pygame.display.set_caption("Geometry Dash")
+screen = pygame.display.set_mode((Ancho, Alto))
+clock = pygame.time.Clock()
+
 
 def mostrar_menu():
-  print("n\=====Menu====")
-  print("1.Jugar")
-  
-  print("2.Salir")
-def main ():
-  while True:
-    mostrar_menu()
-    opcion=input("seleciona una opcion")
+    print("\n===== Menu =====")
+    print("1. Jugar")
+    print("2. Salir")
 
-    if opcion=="1":
-      print("Iniciando el juegon\n")
-      #logica del juego poner aqui
-    elif opcion=="2":
-      print("saliendo del juego ")
-      break
+def main():
+    while True:
+        mostrar_menu()
+        opcion = input("Selecciona una opción: ")
 
-if __name__=="__main":
-  main()
+        if opcion == "1":
+            print("Iniciando el juego...\n")
+            return 
 
-    
+
+
       
+        elif opcion == "2":
+            print("Saliendo del juego...")
+            sys.exit()
+        else:
+            print("Opción no válida")
 
-ejecutando=True
-while ejecutando:
-  for evento in pygame.event.get():
-    if evento.type==pygame.QUIT:
-      ejecutando=False
-  pygame.display.flip()
 
-  clock.tick(Fps)
+def juego():
+    ejecutando = True
+    while ejecutando:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                ejecutando = False
 
-pygame.quit()
+        screen.fill((0, 0, 0))  
+        pygame.display.flip()
+        clock.tick(Fps)
+
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()   
+    juego()  
 
 
 
